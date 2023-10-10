0cab111095f6272214e7608fe0143ac8cfa90c3b
JaviDP03 <javiduarte34@gmail.com>
2023-10-10 12:10:30 +0200

    mods

--- Tema1/src/ejemplos/Ejemplo5.java
+++
@@ -1,7 +0,0 @@
-class Ejemplo5 {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        //Con esta línea puedo imprimir texto.
-        System.out.println("Estoy programando en Java");
-    }
-}
--- Tema1/src/ejemplos/Ejemplo8.java
+++
@@ -1,9 +0,0 @@
-public class Ejemplo8 {
-    //Esta es la clase Ejemplo8.
-    //Hay que escribirla tal cual para que no de fallo.
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        /*Con esta línea se puede imprimir texto.
-        Una vez compilado con "javac" y ejecutado con "java".*/
-    }
-}
