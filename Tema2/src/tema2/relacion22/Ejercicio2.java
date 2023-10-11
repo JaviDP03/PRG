8cada39a872a603c3f1ab89383f6fdc569df8829
usuario <usuario@ALDA-PRO>
2023-10-11 10:26:21 +0200

    corrección

--- Tema2/src/tema2/relacion22/Ejercicio2.java
+++ Tema2/src/tema2/relacion22/Ejercicio2.java
@@ -4,7 +4,8 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		/* Los tipos de datos boolean representan dos valores, verdadero o falso. 
-		 * Crea un programa con dos variables booleanas (o también llamadas lógicas) cada una de ellas con un valor distinto y muestra su contenido por pantalla. */
+		 * Crea un programa con dos variables booleanas (o también llamadas lógicas) cada una de ellas con un valor distinto y 
+		 * muestra su contenido por pantalla. */
 		
 		boolean variable1 = true;
 		boolean variable2 = false;
--- Tema2/src/tema2/relacion22/Ejercicio6.java
+++ Tema2/src/tema2/relacion22/Ejercicio6.java
@@ -4,10 +4,21 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		/* Crea un programa con dos tipos de datos enumerados para las siguientes listas: (1) Otoño, Invierno, Primavera y Verano; 
-		 * (2) África, América, Asia, Europa y Oceanía. Asigna de cada lista un valor a una variable y otro a otra variable. Muestra por pantalla esos valores.
-		 * Sin borrar lo anterior, vuelve a asignar en líneas por debajo dos nuevos valores y muéstralos por pantalla. */
+		 * (2) África, América, Asia, Europa y Oceanía. Asigna de cada lista un valor a una variable y otro a otra variable. 
+		 * Muestra por pantalla esos valores. Sin borrar lo anterior, vuelve a asignar en líneas por debajo dos nuevos valores y muéstralos por pantalla. */
 		
+		enum Estaciones {Otoño, Invierno, Primavera, Verano};
+		enum Continentes {África, America, Asia, Europa, Oceanía};
 		
+		Estaciones estacion1 = Estaciones.Otoño;
+		Continentes continente1 = Continentes.Europa;
+		
+		System.out.println("La estación en " + continente1 + " es " + estacion1 + ".");
+		
+		Estaciones estacion2 = Estaciones.Primavera;
+		Continentes continente2 = Continentes.Oceanía;
+		
+		System.out.println("La estación en " + continente2 + " es " + estacion2 + ".");
 	}
 
 }
