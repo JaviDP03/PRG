19ad2aae24eaebedba8d75e072d6574596e83244
JaviDP03 <javiduarte34@gmail.com>
2023-11-09 19:02:59 +0100

    repaso_bucles

--- Tema3/src/tema3/repasoBucles/Ejercicio10.java
+++ Tema3/src/tema3/repasoBucles/Ejercicio10.java
@@ -6,23 +6,23 @@ public class Ejercicio10 {
 	 */
 	public static void main(String[] args) {
 		// Introducción de variables
-		 boolean esPrimo;
-		
+		boolean esPrimo;
+
 		// Inicialización
 		esPrimo = true;
-		
+
 		// Resultado
 		for (int i = 1; i <= 100; i++) {
-			for (int j = 2; j < i ; j++) {
-				if (i%j == 0) {
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
 					esPrimo = false;
 				}
 			}
-			
+
 			if (esPrimo) {
 				System.out.println(i);
 			}
-			
+
 			esPrimo = true;
 		}
 
